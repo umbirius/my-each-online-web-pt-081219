b554310237ea2d15dace408aae06b5c6e861d072
Umberto Palazzo <upalazzo93@gmail.com>
2019-08-16 19:59:00 +0000

    Done.

--- my_each.rb
+++ my_each.rb
@@ -1,3 +1,16 @@
-def my_each # put argument(s) here
-  # code here
+def my_each (array)
+  i = 0 
+  new_array = []
+  while i < array.length 
+    yield array[i]
+    i +=1 
+    
+  end 
+  array
+end
+
+
+collection = [1, 2, 3, 4]
+my_each(collection) do |i|
+  p i
 end
